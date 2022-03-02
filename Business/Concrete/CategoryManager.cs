e3a4556dfa9116ccedfd974758e319112eb26fcc
Casper <Casper@DESKTOP-ECD7NGQ>
2022-03-02 19:59:44 +0300

    Add project files.

---
+++ Business/Concrete/CategoryManager.cs
@@ -0,0 +1,30 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        ICategoryDal _categoryDdal;
+
+        public CategoryManager(ICategoryDal categoryDdal)
+        {
+            _categoryDdal = categoryDdal;
+        }
+
+        public List<Category> GetAll()
+        {
+            //İş kodları
+            return _categoryDdal.GetAll();
+        }
+
+        public Category GetById(int categoryId)
+        {
+            return _categoryDdal.Get(c => c.CategoryId == categoryId); 
+        }
+    }
+}
