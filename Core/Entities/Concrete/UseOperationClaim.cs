063707ecf03e056d51d51b9ab502d0f283c9e0b0
eren özgün <erennozgunn06@gmail.com>
2022-03-22 13:44:16 +0300

    Security Operations ilk işlemler

---
+++ Core/Entities/Concrete/UseOperationClaim.cs
@@ -0,0 +1,12 @@
+﻿using Core.Entities.Abstract;
+
+namespace Core.Entities.Concrete
+{
+    public class UseOperationClaim : IEntity
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int OperationClaimId { get; set; }
+
+    }
+}
